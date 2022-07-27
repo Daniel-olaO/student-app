b1922e81864899f451a0485097f8df79acb13a0e
Daniel-olaO <danieladedeji135@gmail.com>
2022-07-27 10:52:48 -0400

    Addressed PR comments

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Container, Row, Card} from 'react-bootstrap';
 
-const Loading = ({data}) => {
+const Loading = ({loadingMessage}) => {
   return (
     <Container>
       <Row>
         <Card>
           <Card.Body>
-            <Card.Title>Loading {data}</Card.Title>
+            <Card.Title>Loading {loadingMessage}</Card.Title>
             <Card.Text>Please wait...</Card.Text>
           </Card.Body>
         </Card>
